2594d0f0bb141164359083a6142aa76cf6a223f4
Liam Connor <connor@wcw-staff-145-18-167-232.wireless.uva.nl>
2017-05-03 09:26:31 +0200

    Adding comments

---
+++ single_pulse_ml/plotting-tools.py
@@ -0,0 +1,15 @@
+import matplotlib 
+matplotlib.use('Agg')
+
+import matplotlib.pyplot as plt
+
+def plot_gallery(data_arr, titles, h, w, n_row=3, n_col=4):
+    """Helper function to plot a gallery of portraits"""
+    plt.figure(figsize=(1.8 * n_col, 2.4 * n_row))
+    plt.subplots_adjust(bottom=0, left=.01, right=.99, top=.90, hspace=.35)
+    for i in range(n_row * n_col):
+        plt.subplot(n_row, n_col, i + 1)
+        plt.imshow(data_arr[i].reshape((h, w)), cmap=plt.cm.gray, aspect='auto')
+        plt.title(titles[i], size=14)
+        plt.xticks(())
+        plt.yticks(())
