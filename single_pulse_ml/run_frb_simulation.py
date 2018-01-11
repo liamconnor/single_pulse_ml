4d4a0e82b004cf68148120b276c1622f107b298a
Liam Connor <connor@wcw-staff-145-18-169-196.wireless.uva.nl>
2018-01-11 15:22:42 +0100

    making it more pep8

--- single_pulse_ml/run_frb_simulation.py
+++ single_pulse_ml/run_frb_simulation.py
@@ -1,5 +1,6 @@
 """ Script to build dataset out of simulated 
-	single pulses + false positive triggers
+
+single pulses + false positive triggers
 """
 
 import sim_parameters
@@ -8,20 +9,20 @@ import simulate_frb
 
 # TELESCOPE PARAMETERS:
 freq = (800, 400)   # (FREQ_LOW, FREQ_UP) in MHz
-FREQ_REF = 600      # reference frequency in MHz
-DELTA_T = 0.0016    # time res in seconds
+FREQ_REF = 600      # dispersion reference frequency in MHz
+DELTA_T = 0.0016    # time resolution in seconds
 NAME = "CHIMEPathfinder"
 
 # SIMULATION PARAMETERS 
-NFREQ = 32  # Number of frequencies. Must agree with FP data
-NTIME = 250 # Number of time stamps per trigger
+NFREQ = 32  # Must agree with false-positive data
+NTIME = 250 
 dm = (-0.1, 0.1)
 fluence = (0.005, 0.5)
 width = (2*0.0016, 0.05) # width lognormal dist in seconds
 spec_ind = (-4., 4.)
 disp_ind = 2. 
 scat_factor = (-4., -1.)
-NRFI = 50000
+NRFI = 25000
 SNR_MIN = 3.
 SNR_MAX = 75.0
 out_file_name = None, 
@@ -29,20 +30,21 @@ mk_plot = True
 NSIDE = 8
 dm_time_array = False
 
-fn_rfi = './data/pathfinder_training_data/all_rfi_november17/data_rfi_shuffled.hdf5'
-#fn_rfi = None
+fn_rfi = './data/pathfinder_training_data/\
+            all_rfi_november17/data_rfi_shuffled.hdf5'
+# fn_rfi = None
 
 sim_obj = sim_parameters.SimParams(dm=dm, fluence=fluence,
-                 width=width, spec_ind=spec_ind,
-                 disp_ind=disp_ind, scat_factor=scat_factor, 
-                 SNR_MIN=SNR_MIN, SNR_MAX=SNR_MAX, 
-                 out_file_name=out_file_name, NRFI=NRFI,
-                 NTIME=NTIME, NFREQ=NFREQ, mk_plot=mk_plot, NSIDE=NSIDE)
+                                   width=width, spec_ind=spec_ind,
+                                   disp_ind=disp_ind, scat_factor=scat_factor,
+                                   SNR_MIN=SNR_MIN, SNR_MAX=SNR_MAX,
+                                   out_file_name=out_file_name, NRFI=NRFI,
+                                   NTIME=NTIME, NFREQ=NFREQ,
+                                   mk_plot=mk_plot, NSIDE=NSIDE)
 
-tel_obj = telescope.Telescope(freq=freq, FREQ_REF=FREQ_REF, 
-				    DELTA_T=DELTA_T, name=NAME)
+tel_obj = telescope.Telescope(freq=freq, FREQ_REF=FREQ_REF,
+                              DELTA_T=DELTA_T, name=NAME)
 
 data, labels, params, snr = simulate_frb.run_full_simulation(
-									sim_obj, tel_obj, fn_rfi=fn_rfi, 
-									dm_time_array=dm_time_array)
-
+                                    sim_obj, tel_obj, fn_rfi=fn_rfi,
+                                    dm_time_array=dm_time_array)
