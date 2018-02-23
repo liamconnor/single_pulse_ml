b7d667b525de2d0b701243aa58aebc72a4714189
Liam Connor <connor@wcw-staff-145-109-81-182.wireless.uva.nl>
2018-02-23 14:34:07 +0100

    Now plots FRB candidates after classification

--- single_pulse_ml/plot_tools.py
+++ single_pulse_ml/plot_tools.py
@@ -7,9 +7,42 @@ from matplotlib import pyplot as plt
 # Note that with usetex=True, fonts are rendered with LaTeX.  This may
 # result in an error if LaTeX is not installed on your system.  In that case,
 # you can set usetex to False.
-from astroML.plotting import setup_text_plots
-setup_text_plots(fontsize=8, usetex=True)
+# from astroML.plotting import setup_text_plots
+# setup_text_plots(fontsize=8, usetex=True)
+# #----------------------------------------------------------------------
+# # This function adjusts matplotlib settings for a uniform feel in the textbook.
+# # Note that with usetex=True, fonts are rendered with LaTeX.  This may
+# # result in an error if LaTeX is not installed on your system.  In that case,
+# # you can set usetex to False.
+# from astroML.plotting import setup_text_plots
+# setup_text_plots(fontsize=8, usetex=True)
 
+# #------------------------------------------------------------
+# # Define the distribution parameters to be plotted
+# k_values = [0.5, 1, 2, 2]
+# lam_values = [1, 1, 1, 2]
+# linestyles = ['-', '--', ':', '-.', '--']
+# mu = 0
+# x = np.linspace(-10, 10, 1000)
+
+# #------------------------------------------------------------
+# # plot the distributions
+# fig, ax = plt.subplots(figsize=(5, 3.75))
+
+# for (k, lam, ls) in zip(k_values, lam_values, linestyles):
+#     dist = dweibull(k, mu, lam)
+#     plt.plot(x, dist.pdf(x), ls=ls, c='black',
+#              label=r'$k=%.1f,\ \lambda=%i$' % (k, lam))
+
+# plt.xlim(0, 5)
+# plt.ylim(0, 0.6)
+
+# plt.xlabel('$x$')
+# plt.ylabel(r'$p(x|k,\lambda)$')
+# plt.title('Weibull Distribution')
+
+# plt.legend()
+# plt.show()
 
 try:
     import matplotlib 
@@ -21,18 +54,6 @@ except:
     pass
 
 import keras.backend as backend 
-
-import matplotlib.pyplot as plt
-x = np.arange(1,100.)/50.
-
-def weib(x,n,a):
-    return (a / n) * (x / n)**(a - 1) * np.exp(-(x / n)**a)
-
-count, bins, ignored = plt.hist(np.random.weibull(.25,1000))
-x = np.arange(1,100.)/50.
-scale = count.max()/weib(x, 1., .25).max()
-plt.plot(x, weib(x, 1., 5.)*scale)
-plt.show()
 
 def plot_simulated_events(data, labels, figname,
                           NSIDE, NFREQ, NTIME, cmap='RdBu'):
