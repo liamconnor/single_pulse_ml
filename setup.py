4713dbc8361d882e9549285aff116e1c31c6281b
Liam Connor <connor@wcw-staff-145-109-81-182.wireless.uva.nl>
2018-02-26 08:45:52 +0100

    changing setup function

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readme():
 
 setup(name='single_pulse_ml',
       version='0.1',
-      description='Machine learning implementation of single-pulse search',
+      description='Deep learning implementation of single-pulse search',
       url='http://github.com/liamconnor/single_pulse_ml',
       author='Liam Connor',
       author_email='liam.dean.connor@gmail.com',
