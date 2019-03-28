0c8ef36a12092954b59a4824579be923fdf21c00
Liam Connor <liam.dean.connor@gmail.com>
2019-03-28 10:48:13 +0100

    converted tabs to spaces

--- single_pulse_ml/tests/test_reader.py
+++ single_pulse_ml/tests/test_reader.py
@@ -7,22 +7,22 @@ from single_pulse_ml import reader
 
 class TestReader(TestCase):
 
-	def test_read_hdf5(self):
-		NFREQ = 64
-		NTIME = 250
-		NCANDIDATES = 100
-		data_freq_time = np.random.normal(0, 1, NFREQ*NTIME*NCANDIDATES)
-		data_freq_time.shape = (NCANDIDATES, NFREQ, NTIME)
-		labels = np.ones([NCANDIDATES])
-		fn = './test.hdf5'
+    def test_read_hdf5(self):
+        NFREQ = 64
+        NTIME = 250
+        NCANDIDATES = 100
+        data_freq_time = np.random.normal(0, 1, NFREQ*NTIME*NCANDIDATES)
+        data_freq_time.shape = (NCANDIDATES, NFREQ, NTIME)
+        labels = np.ones([NCANDIDATES])
+        fn = './test.hdf5'
 
-		g = h5py.File(fn,'w')
-		g.create_dataset('data_freq_time', data=data_freq_time)
-		g.create_dataset('labels', data=labels)
-		g.create_dataset('data_dm_time', data=[])				
-		g.close()
-		
-		data_freq, y, data_dm, params = reader.read_hdf5(fn)
+        g = h5py.File(fn,'w')
+        g.create_dataset('data_freq_time', data=data_freq_time)
+        g.create_dataset('labels', data=labels)
+        g.create_dataset('data_dm_time', data=[])               
+        g.close()
+        
+        data_freq, y, data_dm, params = reader.read_hdf5(fn)
                 data_freq, y, data_dm, params, tab = reader.read_hdf5(fn, return_tab=True)
 
 if __name__ == '__main__':
