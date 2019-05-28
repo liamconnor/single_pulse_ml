17801b73e1ac9b77e4c96ac3bd5e1d3f63ae9734
Liam Connor <liam.dean.connor@gmail.com>
2019-05-28 16:01:51 +0200

    back to buffer tests

--- single_pulse_ml/realtime_tools.py
+++ single_pulse_ml/realtime_tools.py
@@ -126,7 +126,8 @@ class RealtimeProc:
             data_tab -= np.median(data_tab)
             data_tab /= np.std(data_tab)
             data_tab[data_tab!=data_tab] = 0.
-            print(maxind, ntime_plot, data_tab.shape)
+            print("maxind, data_tab[:, maxind-ntime_plot//2:maxind+ntime_plot//2].shape, data_classify.shape, data_tab.shape")
+            print(maxind, data_tab[:, maxind-ntime_plot//2:maxind+ntime_plot//2].shape, data_classify.shape, data_tab.shape)
             data_classify[tab] = data_tab[:, maxind-ntime_plot//2:maxind+ntime_plot//2]
 
         return data_classify
