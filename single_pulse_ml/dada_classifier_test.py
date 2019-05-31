05efb875223c06a76951ba29782e7aed58c07d7f
Liam Connor <liam.dean.connor@gmail.com>
2019-05-31 15:02:05 +0200

    fixed single tab dmtrans

--- single_pulse_ml/dada_classifier_test.py
+++ single_pulse_ml/dada_classifier_test.py
@@ -63,7 +63,7 @@ def dada_proc_trigger(reader, nbeam=12):
                                                dm=dm, width=0.001, 
                                                background_noise=data[tab].astype(float),
                                                NTIME=12500, 
-                                               NFREQ=1536, freq=(H.freq_high, H.freq_high-300.))
+                                               NFREQ=1536, freq=(H.freq_high-300., H.freq_high))
         A[A>255] = 255
         A[A<0] = 0
 
@@ -79,10 +79,6 @@ def dada_proc_trigger(reader, nbeam=12):
         if len(data)==0:
             continue
 
-        B = RtProc.dedisperse(data, dm, freq=(H.freq_high, H.freq_high-H.bw))
-        fig = plt.figure()
-        plt.imshow(B, aspect='auto')
-        plt.show()
 
         # This method will rfi clean, dedisperse, and downsample data.
         data_classify, data_dmtime = RtProc.proc_all(data, dm, 
@@ -92,6 +88,10 @@ def dada_proc_trigger(reader, nbeam=12):
                                                      downsample=width, 
                                                      dmtransform=True, 
                                                      freq=(H.freq_high, H.freq_high-H.bw))
+
+        fig = plt.figure()
+        plt.imshow(data_classify, aspect='auto')
+        plt.show()
 
         prob_freqtime = model_freqtime.predict(data_classify[..., None])
         indpmax_freqtime = np.argmax(prob_freqtime[:, 1])
